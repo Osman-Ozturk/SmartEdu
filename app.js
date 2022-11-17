3663d7fb5dc16abcf814d3f77b9c2b81c5da72f3
ztrkosmn09 <ztrkosmn09@gmail.com>
2022-11-17 23:06:26 +0100

    Files wurde angelegt

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+
+// Template Engine 
+app.set('view engine','ejs')
+
+// Middlewares
+app.use(express.static('public'))
+
+
 app.get("/", (req, res) => {
-  res.status (200).send("Hallo");
+  res.status (200).render("index",{page_name:"index"});
 });
+app.get("/about", (req, res) => {
+        res.status (200).render("about",{page_name:"about"});
+      });
 
 app.listen(PORT, () => {
   console.log(`Server führt http://localhost:${PORT} aus`);
